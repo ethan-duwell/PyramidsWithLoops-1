5a8ac72dc09930aa802c499c9a222ea75913c13d
Ethan <Ethan@Ethan-PC.lan1>
2019-09-26 01:21:31 -0400

    Changing Pyramid with for loop
    Changed pyramid to print incremented values on each line
    Changed pyramid to print as a centered pyramid
    Added for loop to change the shape

--- PyramidsWithLoops/src/main/Main.java
+++ PyramidsWithLoops/src/main/Main.java
@@ -1,6 +1,8 @@
 /*
  * Ethan Duwell
  * duwellen@mail.uc.edu
+ * Assignment04
+ * Course: Advanced App Dev Java
  * Printing pyramids using loops. It kind-of works. 
  * What I really want is
         1
@@ -15,11 +17,14 @@ public class Main {
 
 	public static void main(String[] args) {
 		int lines = 5;
-		for (int i = 0; i < lines; i++) {
-			for (int j = 0; j < i+1; j++) {
-				System.out.print(j);
+		for (int i = 1; i <= lines; i++) { //must set less than or equal to 'lines'
+			for (int j = 0; j < lines-i; j++) {
+				System.out.print(" ");
 			}
-			System.out.println("");
+			for (int k=1; k <= i+i - 1; k++) {	//create another loop to create the triangle shape
+				System.out.print(i + "");
+			}
+			System.out.println();
 		}
 	}
 }
